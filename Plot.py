8af8273f8e30632a236621182e909148792e9295
Havukainen Joona J <joona.havukainen@helsinki.fi>
2018-11-13 10:59:07 +0200

    Cleanup of plots

--- Plot.py
+++ Plot.py
@@ -20,13 +20,13 @@ from root_pandas import read_root
 from keras.models import Model, load_model
 from scipy.stats import sem
 
-model = load_model('checkpoint_model.h5')
+model = load_model('checkpoint_model_tmp.h5')
 test_data = read_root("test_data.root")
 
 Ccand_variables = list(test_data.filter(regex='jetPF_chg_'))
 Ncand_variables = list(test_data.filter(regex='jetPF_neu_'))
 Pcand_variables = list(test_data.filter(regex='jetPF_pho_'))
-Global_variables = list(set(list(test_data.filter(regex='jet')))-set(Ccand_variables+Ncand_variables+Pcand_variables))
+Global_variables = list(set(list(test_data.filter(regex='jet')))-set(Ccand_variables+Ncand_variables+Pcand_variables))+list(test_data.filter(regex='QG_'))
 Gen_variables = list(test_data.filter(regex='genJet'))
 Flavor_variables = list(test_data.filter(regex='isPhys'))
 
@@ -59,11 +59,14 @@ test_Ncands = np.reshape(np.array(test_Ncands), (test_Ncands.shape[0], n_particl
 test_Pcands = np.reshape(np.array(test_Pcands), (test_Pcands.shape[0], n_particles, test_Pcands.shape[1]/n_particles))
 
 test_data['predictions'] = model.predict([test_Ccands, test_Ncands, test_Pcands, test_Globals])
+print test_data['predictions'][:5]
 
 # Assuming learning target was the correction factor
 test_data['Response'] = test_data['jetPt']/test_data['genJetPt']
 test_data['jetPt_DNN'] = test_data['jetPt']*test_data['predictions']
-test_data['Response_DNN'] = test_data['DNN_corr_jet_pT']/test_data['genJetPt']
+test_data['Response_DNN'] = test_data['jetPt_DNN']/test_data['genJetPt']
+
+print test_data['Response_DNN'][:5]
 
 # List of pairs of variables to plots, comparing the jet corr pT and the DNN corrected pT
 to_plot = [('genJetPt', 'Response'),
@@ -74,10 +77,10 @@ to_plot = [('genJetPt', 'Response'),
 to_histogram = ['Response']
 
 # Dictionary for binning the different variables
-bin_dict = {'QG_ptD':np.arange(0.1, 1.05, 0.05),
+bin_dict = {'QG_ptD':np.arange(0.15, 1.00, 0.05),
             'QG_axis2':np.arange(0.0, 0.15, 0.005),
-            'QG_mult':np.arange(0.0, 81.0, 1.0),
-            'genJetPt':np.arange(0.0, 1020.0, 20.0)}
+            'QG_mult':np.arange(1.0, 81.0, 1.0),
+            'genJetPt':np.arange(80.0, 1020.0, 20.0)}
 yrange_dict = {'Response':[0.8, 1.2]}
 
 for variables in to_plot:
@@ -91,11 +94,11 @@ for variables in to_plot:
     errors_l1l2l3 = np.zeros(len(binning))
     errors_DNN = np.zeros(len(binning))
 
-    for i in range(1, len(binning)):
+    for i in range(1, len(binning)+1):
         means_l1l2l3[i-1] = np.mean(test_data[variables[1]][indices == i])
         means_DNN[i-1] = np.mean(test_data[variables[1]+'_DNN'][indices == i])
-        errors_l1l2l3[i-1] = sem(test_data[variables[1]][indices == i])
-        errors_DNN[i-1] = sem(test_data[variables[1]][indices == i])
+        errors_l1l2l3[i-1] = np.std(test_data[variables[1]][indices == i])
+        errors_DNN[i-1] = np.std(test_data[variables[1]][indices == i])
 
     # Take care of possible nans, due to empty bins
     means_l1l2l3 = np.nan_to_num(means_l1l2l3)
@@ -103,12 +106,17 @@ for variables in to_plot:
     errors_l1l2l3 = np.nan_to_num(errors_l1l2l3)
     errors_DNN = np.nan_to_num(errors_DNN)
 
-    plt.scatter(x, means_l1l2l3, label='L1L2L3', color='blue')
+    print means_DNN
+
+#    plt.errorbar(x, means_l1l2l3, yerr=errors_l1l2l3, label='L1L2L3', color='blue', fmt='.')
+#    plt.errorbar(x, means_DNN, yerr=errors_DNN, label='DNN', color='green', fmt='.')
+
+    plt.scatter(x, means_l1l2l3, label='L1L2L3', color='blue', s=2)
     plt.fill_between(x, means_l1l2l3-errors_l1l2l3, means_l1l2l3+errors_l1l2l3,
-                     alpha=0.4, label='$\pm 1\sigma$', color='green')
-    plt.scatter(x, means_DNN, label='DNN', color='orange')
+                     alpha=0.4, label='$\pm 1\sigma$', color='blue')
+    plt.scatter(x, means_DNN, label='DNN', color='orange', s=2)
     plt.fill_between(x, means_DNN-errors_DNN, means_DNN+errors_DNN,
-                     alpha=0.4, label='$\pm 1\sigma$', color='green')
+                     alpha=0.4, label='$\pm 1\sigma$', color='orange')
 
     plt.legend()
     plt.plot([0, (binning[-1]+step)], [1, 1], 'k--')
@@ -117,3 +125,9 @@ for variables in to_plot:
     plt.xticks(binning[::4])
     plt.ylabel(variables[1])
     plt.xlabel(variables[0])
+
+    plt.savefig(variables[1]+'_vs_'+variables[0]+'.pdf')
+    plt.clf()
+
+
+
